661f1c693dd2a5f8773c24f52aba3e4ee67af7b2
Daniel Brian <db9755949@gmail.com>
2024-10-04 11:48:59 +0300

    ADD: ICRUD Products API

--- Personal/app.js
+++ Personal/app.js
@@ -1,27 +1,16 @@
-const http = require("http");
-const express = require("express")
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/',(req, res, next) => {
-    console.log('This always run!');
-    next();
-});
+// it will call next in the end so that the request reaches our middleware
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product',(req, res, next) => {
-    console.log('In another middlware!');
-    res.send('<h1>The "Add Product" Page</h1>');
-});
-
-app.post('/product',(req,res, next) => {
-    console.log(req.body);
-    res.send("product")
-})
-
-app.use('/',(req, res, next) => {
-    console.log('In another middlware!');
-    res.send('<h1>Hello from Express</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3001,() => {
     console.log("Server running at port 3001...");
